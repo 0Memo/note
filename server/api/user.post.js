0bb96a337e1676cd7e85d268df4bf988180c5728
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-04-07 17:53:46 +0200

    updated schema.prisma, user.post.js & updated migrationto ensure password hashing is functional

--- server/api/user.post.js
+++ server/api/user.post.js
@@ -1,14 +1,20 @@
 // /api/user POST
 import prisma from "../utils/prisma";
+import bcrypt from 'bcryptjs';
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
+
+    const salt = await bcrypt.genSalt(10);
+
+    const passwordHash = await bcrypt.hash(body.password, salt);
 
     try {
         const user = await prisma.user.create({
             data: {
             email: body.email,
-            password: body.password,
+            password: passwordHash,
+            salt: salt,
             },
         });
 
