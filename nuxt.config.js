72e9605e5b2dac4bbbac96d8bf96125a244dc171
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-04-09 21:20:06 +0200

    updated prisma.js, vercel.json & nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
   modules: [
     '@prisma/nuxt',
     '@vueuse/nuxt'
   ],
+  build: {
+    transpile: ['@prisma/client'],
+  }
 })
