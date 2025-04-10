5570b2e6bce0b497fe41dd8f61ca2259db0b64a5
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-04-10 10:45:24 +0200

    updated nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,35 +1,12 @@
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
   css: ["~/assets/main.css"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
   modules: ["@vueuse/nuxt"],
   nitro: {
     externals: {
       external: ["@prisma/client", ".prisma/client"],
     },
-    moduleSideEffects: ["@prisma/client"],
-    // Optional: Ensuring proper static deployment for Nuxt 3
-    prerender: {
-      // Add any routes you want to be prerendered here
-      routes: ["/"],
-    },
   },
-  // Optionally, set server-side configurations or routes
-  server: {
-    // Ensure Nuxt is properly running on Vercel's serverless environment
-    port: process.env.PORT || 3000,
-  },
-
-  // Optional: Build configurations for optimizations
   build: {
-    // Example to optimize the build
-    analyze: process.env.NODE_ENV === "production",
-    transpile: ["@prisma/client"], // Ensure Prisma is transpiled on serverless environments
+    transpile: ["@prisma/client"],
   },
 });
