ce1f7b930ce9e06be3b94250fbfced5f1df0d488
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-04-16 09:02:45 +0200

    updated app.vue

--- app.vue
+++ app.vue
@@ -7,4 +7,9 @@
 <script setup>
   // You can directly import global CSS here as well if needed
   import '~/assets/styles/global.css';
+  useHead({
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  })
 </script>
