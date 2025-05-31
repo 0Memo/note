8ec2f8974537227e30ac6cac68b82f81cf67ebf3
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-05-31 11:12:42 +0200

    updated privacy.vue

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -68,7 +68,7 @@
             <div class="mt-6">
                 <nuxt-link
                     :to="localePath('/')"
-                    class="w-full mt-4 bg-purple-900 text-lg px-4 py-2 rounded-2xl font-bold space-x-4 inline-flex justify-center items-center"
+                    class="w-full mt-4 bg-purple-900 text-lg px-4 py-2 rounded-2xl font-bold space-x-4 inline-flex justify-center items-center mb-8"
                 >
                     <span>{{ $t('modal.privacy.back') }}</span>
                     <span>➤</span>
