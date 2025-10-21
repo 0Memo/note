b90b81c929baf445d60d06afd2ea0eb1e2ed5763
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-10-21 09:52:11 +0200

    updated files structure

---
+++ components/Toucan.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class='head'>
+        <div class='start'></div>
+        <div class='colorful'></div>
+        <div class='eye'></div>
+    </div>
+    <div class='body'>
+        <div class='back'></div>
+        <div class='neck'></div>
+    </div>
+    <div class='branch'>
+        <div class='bigLeaf'></div>
+        <div class='smallLeaf'></div>
+        <div class='stem'></div>
+    </div>
+    <div class='tail'></div>
+</template>
