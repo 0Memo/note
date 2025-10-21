cab578120056150bcee42126cfb1d07e6ef9b94e
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-10-21 17:22:19 +0200

    updated svgs

---
+++ components/Skull.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="craneo">
+        <div class="skull">
+            <div class="front">
+            <span class="cruz"></span>
+            </div>
+            <div class="cempasuchil">
+            <div class="eye">
+            </div>
+            </div>
+
+            <div class="cempasuchil">
+            <div class="eye">
+            </div>
+            </div>
+
+            <div class="nose"></div>
+
+            <div class="dientes">
+            <div class="dientes-2">
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
